da3dc2577ce4f5e2454b6d5b99ae773868e5909e
Milky0-Oway <milka.korotkaya@gmail.com>
2024-05-26 22:16:11 +0300

    fixes3.0

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const path = require('path');
 const db = require('./database');
 
 const PORT = process.env.PORT || 5000;
-/*
-const PORT = process.env.PORT || 80;
-*/
 
 const app = express();
 const server = http.createServer(app);
@@ -126,7 +123,7 @@ io.on('connection', socket => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
