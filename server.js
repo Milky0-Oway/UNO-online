bc86390725755e7e4894137da590b36a339935e0
Milky0-Oway <milka.korotkaya@gmail.com>
2024-05-27 00:25:21 +0300

    fixes7.0

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { addUser, getUsersInRoom, getUser, removeUser } = require('./users');
 const path = require('path');
 const db = require('./database');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const app = express();
 const server = http.createServer(app);
